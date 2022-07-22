c537cb06f86200f525bf56b887dc8c41bce89417
Julian <mayorga.julian@gmail.com>
2022-07-22 14:48:31 -0300

    Mock GraphQL request in test using msw

--- README.md
+++ README.md
@@ -116,3 +116,38 @@ describe("Home", () => {
 ```
 
 10. `yarn test`
+11. `yarn add msw --dev`
+12. Mock GraphQL query in `__tests__/index.test.tsx`
+
+```tsx
+import { render, screen, waitFor } from "@testing-library/react";
+import Home from "../pages/index";
+import { graphql } from "msw";
+import { setupServer } from "msw/node";
+
+const server = setupServer(
+  graphql.query("GetCartById", (req, res, ctx) => {
+    return res(
+      ctx.data({
+        cart: { totalItems: 10 },
+      })
+    );
+  })
+);
+
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
+
+describe("Home", () => {
+  it("renders total items", async () => {
+    render(<Home />);
+
+    const totalItems = await waitFor(() => screen.getByText("Total items: 10"));
+
+    expect(totalItems).toBeInTheDocument();
+  });
+});
+```
--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,11 +1,29 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Home from "../pages/index";
+import { graphql } from "msw";
+import { setupServer } from "msw/node";
+
+const server = setupServer(
+  graphql.query("GetCartById", (req, res, ctx) => {
+    return res(
+      ctx.data({
+        cart: { totalItems: 10 },
+      })
+    );
+  })
+);
+
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
 
 describe("Home", () => {
-  it("renders total items", () => {
+  it("renders total items", async () => {
     render(<Home />);
 
-    const totalItems = screen.getByText("Total items:");
+    const totalItems = await waitFor(() => screen.getByText("Total items: 10"));
 
     expect(totalItems).toBeInTheDocument();
   });
